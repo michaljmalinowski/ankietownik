85a8719c70dcdb4f75b3a1f19c4a5ffdc302d160
michaljmalinowski <michaljmalinowski@github.com>
2020-05-11 20:53:34 +0200

    RegistrationService - default implementation

---
+++ src/main/java/malinowski/michal/ankietownik/service/registration/RegistrationServiceImpl.java
@@ -0,0 +1,14 @@
+package malinowski.michal.ankietownik.service.registration;
+
+import lombok.extern.slf4j.Slf4j;
+import malinowski.michal.ankietownik.service.registration.RegistrationData.RegistrationData;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RegistrationServiceImpl implements RegistrationService {
+    @Override
+    public void register(RegistrationData registrationData) {
+        //na podstawie repozytorium
+    }
+}
