9f74fc6166d43d602ce61f7a8dcd27d63b6e1ac1
michaljmalinowski <michaljmalinowski@github.com>
2020-05-11 20:52:50 +0200

    RegistrationService - interface

---
+++ src/main/java/malinowski/michal/ankietownik/service/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package malinowski.michal.ankietownik.service.registration;
+
+import malinowski.michal.ankietownik.service.registration.RegistrationData.RegistrationData;
+
+public interface RegistrationService {
+    void register(RegistrationData registrationData);
+}
